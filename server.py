35f64468c830451a2341f2596311ffd4a14cc568
Peter Teixeira <PtrTeixeira@gmail.com>
2020-04-16 22:31:49 -0400

    Implement Python server
    Write the server in Python. This is *definitely* not a strength of
    Python's core library.

---
+++ server.py
@@ -0,0 +1,21 @@
+import http.server
+import socketserver
+
+PORT = 3000
+
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path != "/":
+            self.send_response(404)
+            return
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(bytes("Hello World!\n", 'utf-8'))
+        return
+
+Handler = RequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Listening on port", PORT)
+    httpd.serve_forever()
